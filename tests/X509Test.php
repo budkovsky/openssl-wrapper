5445e462f6134852d18c1d580c01bb4fd672ad99
piotr <piotr@nowhere.com>
2019-08-31 15:27:00 +0200

    X509 tests in progress

--- tests/X509Test.php
+++ tests/X509Test.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\TestCase;
 use Budkovsky\OpenSslWrapper\X509;
 use Budkovsky\OpenSslWrapper\PrivateKey;
 use Budkovsky\OpenSslWrapper\Tests\Helper\CsrTestHelper;
+use Budkovsky\OpenSslWrapper\Tests\Helper\X509TestHelper;
 
 class X509Test extends TestCase
 {
@@ -23,14 +24,57 @@ class X509Test extends TestCase
             X509::class,
             new X509(CsrTestHelper::getCsrExample($privateKey), $privateKey)
         );
-//         $anotherPrivateKey = PrivateKey::create();
-//         $this->assertInstanceOf(
-//             X509::class,
-//             X509::create(
-//                 CsrTestHelper::getCsrExample($anotherPrivateKey),
-//                 $anotherPrivateKey
-//             )
-//         );
+        $anotherPrivateKey = PrivateKey::create();
+        $this->assertInstanceOf(
+            X509::class,
+            X509::create(CsrTestHelper::getCsrExample($anotherPrivateKey), $anotherPrivateKey)
+        );
     }
 
+    public function testCanBeCreatedFromString(): void
+    {
+        $body = file_get_contents(__DIR__.'/asset/x509example.cert');
+        $x509 = X509::create()->load($body);
+        $this->assertInstanceOf(X509::class, $x509);
+        $this->assertEquals($body, $x509->export());
+    }
+
+    public function testCanExport(): void
+    {
+        $export = X509TestHelper::getX509example()->export();
+        $this->assertIsString($export);
+        $this->assertNotEmpty($export);
+    }
+
+    public function testCanExportToFile(): void
+    {
+        $x509 = X509TestHelper::getX509example();
+        $filePath = sprintf(
+            '%s/%s.cert',
+            sys_get_temp_dir(),
+            bin2hex(openssl_random_pseudo_bytes(10))
+        );
+        $x509->exportToFile($filePath);
+        $this->assertFileExists($filePath);
+        $this->assertNotEmpty(file_get_contents($filePath));
+        $this->assertEquals($x509->export(), file_get_contents($filePath));
+    }
+
+    public function testCanExportToPkcs12(): void
+    {
+        //TODO tests
+    }
+
+    public function testCanExportToPkcs12File(): void
+    {
+        //TODO tests
+    }
+
+    public function testCanGetFingerprint(): void
+    {
+        $x509 = X509TestHelper::getX509example();
+        $this->assertIsString($x509->getFingerprint());
+        $this->assertEquals(40, strlen($x509->getFingerprint('sha1')));
+        $this->assertEquals(32, strlen($x509->getFingerprint('md5')));
+    }
 }
